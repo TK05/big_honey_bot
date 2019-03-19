3d950f39505e19cb6a58f016b13cb289bf4897f5
Thomas Kelley <TKelley05@gmail.com>
2019-03-18 21:32:01 -0700

    Made sidebar logic a bit smarter

--- sidebar/update_sidebar.py
+++ sidebar/update_sidebar.py
@@ -33,6 +33,7 @@ def update_record():
     for team in id_response['league']['standard']:
         if team['nickname'] == TEAM:
             team_focus_id = team['teamId']
+            team_conf = team['confName']
             break
 
     rec_response = requests.get("https://data.nba.net/prod//v1/current/standings_conference.json").json()
@@ -45,7 +46,7 @@ def update_record():
                 conf_data = conf
                 team_seed = seed
 
-    return conf_data, team_seed, f"Record: {tf_wins} - {tf_loss}"
+    return conf_data, team_seed, f"Record: {tf_wins} - {tf_loss} | #{team_seed + 1} in the {team_conf}"
 
 
 def update_tripdub():
@@ -74,20 +75,36 @@ def update_playoff(conf_data, team_seed):
 
     tf_wins = int(conf_data[team_seed]['win'])
     ninth_losses = int(conf_data[8]['loss'])
+    po_code_1 = conf_data[team_seed]['clinchedPlayoffsCode']    # 'P' for playoffs?
+    po_code_2 = conf_data[team_seed]['clinchedPlayoffsCode']    # 'x' for playoffs?
     play_magic_num = 83 - tf_wins - ninth_losses
 
-    if play_magic_num > 1:  # TODO: manually adjusted
-        play_sub = f'Playoff Magic #: {play_magic_num}^*'
+    if po_code_1 != '':
+        playoffs_clinched = True
     else:
+        playoffs_clinched = False
+
+    if playoffs_clinched:
         play_sub = f'Playoffs **CLINCHED!**'
-
-    next_seed_losses = int(conf_data[int(team_seed) + 1]['loss'])
-    seed_magic_num = 83 - tf_wins - next_seed_losses
-
-    if seed_magic_num > 0:
-        seed_sub = f'#{int(team_seed) + 1} Seed Magic #: {seed_magic_num}^*'
+        for seed in range(7, team_seed, -1):
+            seed_losses = int(conf_data[seed]['loss'])
+            seed_magic_num = 83 - tf_wins - seed_losses
+            seed_sub = f'#{seed} Seed Magic #: {seed_magic_num}^*'
+            if seed_magic_num > 0:
+                break
+            else:
+                seed_sub = f'#{team_seed + 1} Seed: CLINCHED!'
     else:
-        seed_sub = f'#{int(team_seed) + 1} Seed **CLINCHED!**'
+        play_sub = f'Playoff Magic #: {play_magic_num}^*'
+        seed_sub = ''
+
+    # next_seed_losses = int(conf_data[int(team_seed) + 1]['loss'])
+    # seed_magic_num = 83 - tf_wins - next_seed_losses
+    #
+    # if seed_magic_num > 0:
+    #     seed_sub = f'#{int(team_seed) + 1} Seed Magic #: {seed_magic_num}^*'
+    # else:
+    #     seed_sub = f'#{int(team_seed) + 1} Seed **CLINCHED!**'
 
     return play_sub, seed_sub
 
