39e4ba6868e825abc1496c3ddefe4f2dc99875a0
Thomas Kelley <TKelley05@gmail.com>
2019-06-13 11:45:37 -0700

    Moved gists data into config

---
+++ config.py
@@ -0,0 +1,14 @@
+gists = {
+    'pre': {
+        'filename': 'pre_game.md',
+        'id': '29116608ab19125f97f40fea9c6582a9'
+    },
+    'game': {
+        'filename': 'game_thread.md',
+        'id': 'd0fbf3fce1e517c5b567e87d5b7b2542'
+    },
+    'post': {
+        'filename': 'post_game.md',
+        'id': '1a32b36af1a2c6ed3eb16f08ffa51580'
+    }
+}
