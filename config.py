53fc783a1a68463cdc90261d74c730cad5d6b2b0
Thomas Kelley <TKelley05@gmail.com>
2019-10-28 21:55:33 -0700

    Update config

--- config.py
+++ config.py
@@ -9,16 +9,16 @@ setup = {
     'espn_url': 'http://www.espn.com/nba/team/schedule/_/name/den',  # ESPN schedule hub for team
     'nba_url': 'https://www.nba.com/nuggets/schedule',  # nba.com team schedule
     'user_agent': 'BigHoneyBot_by_TK05',
-    # 'flairs': {     # Subreddit specific link_flair IDs
-    #     'pre': '51a03fc6-3e0c-11e9-8856-0e97475b835e',
-    #     'game': 'b94d2b1e-7af0-11e8-9106-0ecef3c85830',
-    #     'post': '3f0b88d4-3e0c-11e9-afb3-0e6687893da2'
-    # },
-    'flairs': {   # Test flairs
-        'pre': '701ba516-3e0d-11e9-80e7-0e07faed2e96',
-        'game': '7fa2da68-3e0d-11e9-9571-0e5fef20b374',
-        'post': '7962e238-3e0d-11e9-92dc-0edef102ea7a'
-    }
+    'flairs': {     # Subreddit specific link_flair IDs
+        'pre': '51a03fc6-3e0c-11e9-8856-0e97475b835e',
+        'game': 'b94d2b1e-7af0-11e8-9106-0ecef3c85830',
+        'post': '3f0b88d4-3e0c-11e9-afb3-0e6687893da2'
+    },
+    # 'flairs': {   # Test flairs
+    #     'pre': '701ba516-3e0d-11e9-80e7-0e07faed2e96',
+    #     'game': '7fa2da68-3e0d-11e9-9571-0e5fef20b374',
+    #     'post': '7962e238-3e0d-11e9-92dc-0edef102ea7a'
+    # }
 }
 
 options = {
