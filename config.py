90b99e04796253208b7c4720506382d8fdea967b
Thomas Kelley <6148136+TK05@users.noreply.github.com>
2021-10-17 20:34:16 -0700

    update config and requirements

--- config.py
+++ config.py
@@ -16,29 +16,10 @@ setup = {
         'pre': '51a03fc6-3e0c-11e9-8856-0e97475b835e',
         'game': 'b94d2b1e-7af0-11e8-9106-0ecef3c85830',
         'post': '3f0b88d4-3e0c-11e9-afb3-0e6687893da2'
-    },
+    }
     # 'flairs': {   # Test flairs
     #     'pre': '701ba516-3e0d-11e9-80e7-0e07faed2e96',
     #     'game': '7fa2da68-3e0d-11e9-9571-0e5fef20b374',
     #     'post': '7962e238-3e0d-11e9-92dc-0edef102ea7a'
     # }
-}
-
-gists = {
-    'pre': {
-        'filename': 'pre_game.md',
-        'id': '29116608ab19125f97f40fea9c6582a9'
-    },
-    'game': {
-        'filename': 'game_thread.md',
-        'id': 'd0fbf3fce1e517c5b567e87d5b7b2542'
-    },
-    'post': {
-        'filename': 'post_game.md',
-        'id': '1a32b36af1a2c6ed3eb16f08ffa51580'
-    },
-    'schedule': {
-        'filename': 'schedule.json',
-        'id': 'ca4c2fa43d1da9986d2c66556774f44b'
-    }
 }
