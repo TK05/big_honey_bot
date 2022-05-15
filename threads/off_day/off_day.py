f93dec63131e3592333851a56a78e848bc52ef66
Thomas Kelley <6148136+TK05@users.noreply.github.com>
2022-05-15 12:52:21 -0700

    get playoff data from espn

--- threads/off_day/off_day.py
+++ threads/off_day/off_day.py
@@ -8,8 +8,8 @@ from config import setup
 from tools.toolkit import description_tags
 
 
-def generate_thread_body(event):
-    """Generates off day thread body based on that days games. Replaces placeholder tags with
+def generate_thread_body(event=None):
+    """Generates off day thread body based on that days games (from ESPN). Replaces placeholder tags with
     this generated data.
 
     :param event: Event to generate thread body for
@@ -17,28 +17,31 @@ def generate_thread_body(event):
     :returns: Nothing, modifies event in place
     :rtype: None
     """
-    req = requests.get('https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json').json()
-    games = sorted(req['scoreboard']['games'], key=lambda i: i['gameId'])
+    req = requests.get('https://site.web.api.espn.com/apis/v2/scoreboard/header?sport=basketball&league=nba&region=us&lang=en&contentorigin=espn&buyWindow=1m&showAirings=buy,live,replay&showZipLookup=true&tz=America/New_York').json()
+    games = sorted(req['sports'][0]['leagues'][0]['events'], key=lambda i: i['id'])
 
     body = f"|Today's Games||||\n" \
            f"|:--|:--|:--|:--|\n"
 
     for game in games:
-        time = datetime.strptime(game['gameTimeUTC'], '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=pytz.UTC)
+        time = datetime.strptime(game['date'], '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=pytz.UTC)
         time_local = f"{datetime.strftime(time.astimezone(pytz.timezone(setup['timezone'])), format('%#I:%M %p %Z'))}"
         time_link = f"https://dateful.com/time-zone-converter?t=" \
                     f"{datetime.strftime(time.astimezone(pytz.timezone(setup['timezone'])), format('%#I:%M %p'))}" \
                     f"&tz={setup['location']}&"
         time_fmt = f"[{time_local}]({time_link})"
-        teams = f"{game['awayTeam']['teamName']}({game['awayTeam']['seed']}) @ {game['homeTeam']['teamName']}({game['homeTeam']['seed']})"
-        series = f"{game['seriesGameNumber']}, {game['seriesText']}"
-        nba_link = f"[nba.com](https://www.nba.com/game/{game['gameId']})"
+        teams = f"{game['name']}"
+        series = f"{game['note']}, {game['seriesSummary']}"
+        nba_link = f"[ESPN](https://www.espn.com/nba/game?gameId={game['id']})"
         body += f"|{teams}|{time_fmt}|{series}|{nba_link}|\n"
 
     if len(games) == 0:
         body = "No games scheduled today"
 
-    event.body = event.body.replace(description_tags['daily_games'], f"{body}")
+    if not event:
+        print(body)
+    else:
+        event.body = event.body.replace(description_tags['daily_games'], f"{body}")
 
 
 def off_day_thread_handler(event):
@@ -54,3 +57,7 @@ def off_day_thread_handler(event):
 
     print(f"{os.path.basename(__file__)}: Created headline: {event.summary}")
     new_thread(event)
+
+
+if __name__ == "__main__":
+    generate_thread_body()
