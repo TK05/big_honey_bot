7f6dcf8fbd5a5256a894c4e310a3afde0e95ab84
Thomas Kelley <6148136+TK05@users.noreply.github.com>
2022-06-12 17:08:11 -0700

    support for static offseason events

--- threads/off_day/off_day.py
+++ threads/off_day/off_day.py
@@ -8,18 +8,10 @@ from bots.thread_handler_bot import new_thread
 from config import setup
 from tools.toolkit import description_tags
 from data.static.data import lookup_by_loc
+from threads.off_day.static_events import events as se
 
 
-def generate_thread_body(event=None):
-    """Generates off day thread body based on that days games (from ESPN). Replaces placeholder tags with
-    this generated data.
-
-    :param event: Event to generate thread body for
-    :type event: gcsa.event.Event
-    :returns: Nothing, modifies event in place
-    :rtype: None
-    """
-
+def get_espn_games():
     response = requests.get('https://www.espn.com/nba/schedule').text
     response_selector = Selector(text=response)
     games_raw = response_selector.xpath('.//div[@class="mt3"]/div')
@@ -37,7 +29,8 @@ def generate_thread_body(event=None):
 
         date_str = game.xpath('.//div[@class="Table__Title"]/text()').get().strip()
         time_str = game_base[2].xpath('.//a[@class="AnchorLink"]/text()').get()
-        time = pytz.timezone('US/Eastern').localize(datetime.strptime(f"{date_str} {time_str}", '%A, %B %d, %Y %I:%M %p'))
+        time = pytz.timezone('US/Eastern').localize(
+            datetime.strptime(f"{date_str} {time_str}", '%A, %B %d, %Y %I:%M %p'))
         time_tz = time.astimezone(pytz.timezone(setup['timezone']))
 
         try:
@@ -67,7 +60,8 @@ def generate_thread_body(event=None):
             tv = net_con.xpath('.//div[contains(@class, "network-name")]/text()').get()
 
         if time_tz.date() == datetime.now(tz=pytz.timezone(setup['timezone'])).date():
-            games_today.append(f"|{away_team} {lookup_by_loc[away_team][0]} @ {home_team} {lookup_by_loc[home_team][0]}|{time_fmt}|{game_note}|{tv}|{espn_link}|\n")
+            games_today.append(
+                f"|{away_team} {lookup_by_loc[away_team][0]} @ {home_team} {lookup_by_loc[home_team][0]}|{time_fmt}|{game_note}|{tv}|{espn_link}|\n")
         else:
             # Catch when home or away team is none/tbd
             if not home_team:
@@ -77,23 +71,75 @@ def generate_thread_body(event=None):
                 body_rows.append(
                     f"|TBD @ {home_team} {lookup_by_loc[home_team][0]}|{time_fmt}|{game_note}|{tv}|{espn_link}|\n")
             else:
-                body_rows.append(f"|{away_team} {lookup_by_loc[away_team][0]} @ {home_team} {lookup_by_loc[home_team][0]}|{time_fmt}|{game_note}|{tv}|{espn_link}|\n")
+                body_rows.append(
+                    f"|{away_team} {lookup_by_loc[away_team][0]} @ {home_team} {lookup_by_loc[home_team][0]}|{time_fmt}|{game_note}|{tv}|{espn_link}|\n")
+
+    return body_rows, games_today
+
+
+def generate_thread_body(event=None, in_playoffs=True, include_static=False):
+    """Generates off day thread body based on that days games. Replaces placeholder tags with
+    this generated data.
+
+    :param event: Event to generate thread body for
+    :type event: gcsa.event.Event
+    :param in_playoffs: True will include upcoming playoff games
+    :type in_playoffs: bool
+    :param include_static: True includes static events from threads.static_events dict
+    :type include_static: bool
+    :returns: Nothing, modifies event in place
+    :rtype: None
+    """
+
+    body = ""
 
     # build body based on today and upcoming games
-    body = ""
-    if games_today:
-        body += f"|Today's Games|||||\n" \
-               f"|:--|:--|:--|:--|:--|\n"
-        for game in games_today:
-            body += f"{game}"
-        body += "\n&nbsp;\n\n"
-    else:
-        body += "No games scheduled today\n\n&nbsp;\n\n"
-    if body_rows:
-        body += f"|Upcoming Games|||||\n" \
-               f"|:--|:--|:--|:--|:--|\n"
-        for game in body_rows:
-            body += f"{game}"
+    if in_playoffs:
+        upcoming_games, games_today = get_espn_games()
+
+        if games_today:
+            body += f"|Today's Games|||||\n" \
+                   f"|:--|:--|:--|:--|:--|\n"
+            for game in games_today:
+                body += f"{game}"
+            body += "\n&nbsp;\n\n"
+        else:
+            body += "No games scheduled today\n\n&nbsp;\n\n"
+        if upcoming_games:
+            body += f"|Upcoming Games|||||\n" \
+                   f"|:--|:--|:--|:--|:--|\n"
+            for game in upcoming_games:
+                body += f"{game}"
+
+    # build second table of static events if include_static
+    if include_static:
+        body += f"\n&nbsp;\n\n" \
+                f"|Upcoming Events|||\n" \
+                f"|:--|:--|:--|\n"
+
+        def format_links(links):
+            return ', '.join(f"[{k}]({v})" for k, v in links.items())
+
+        events_today = []
+        current_doy = int(datetime.strftime(datetime.now(), "%j"))
+
+        for i in sorted(se.keys()):
+            if se[i]["start_doy"] <= current_doy <= se[i]["end_doy"]:
+                events_today.append(f"|{se[i]['desc']}|{se[i]['date_str']}|{format_links(se[i]['links'])}|\n")
+            elif i < current_doy:
+                pass
+            else:
+                body += f"|{se[i]['desc']}|{se[i]['date_str']}|{format_links(se[i]['links'])}|\n"
+
+        if events_today:
+            top = f"|Today's Events|||\n" \
+                  f"|:--|:--|:--|\n"
+            for ev in events_today:
+                top += f"{ev}"
+
+            body = f"{top}" \
+                   f"\n&nbsp;\n\n" \
+                   f"{body}"
 
     if not event:
         print(body)
@@ -110,7 +156,7 @@ def off_day_thread_handler(event):
     :rtype: NoneType
     """
 
-    generate_thread_body(event)
+    generate_thread_body(event, in_playoffs=True, include_static=True)
 
     print(f"{os.path.basename(__file__)}: Created headline: {event.summary}")
     new_thread(event)
---
+++ threads/off_day/static_events.py
@@ -0,0 +1,35 @@
+# datetime.strftime(datetime.strptime("July 02, 2022", "%B %d, %Y"), "%j")
+events = {
+    174: {
+        "start_doy": 174,
+        "end_doy": 174,
+        "date_str": "June 23",
+        "desc": "NBA Draft",
+        "links": {
+            "nba.com": "https://www.nba.com/draft/2022"
+        }
+     },
+    183: {
+        "start_doy": 183,
+        "end_doy": 184,
+        "date_str": "July 2-3",
+        "desc": "California Classic in San Francisco – (Lakers, Heat, Kings, Warriors)",
+        "links": {}
+    },
+    186: {
+        "start_doy": 186,
+        "end_doy": 188,
+        "date_str": "July 5-7",
+        "desc": "SLC Summer League - (Grizzlies, Thunder, 76ers, Jazz)",
+        "links": {}
+    },
+    188: {
+        "start_doy": 188,
+        "end_doy": 198,
+        "date_str": "July 7-17",
+        "desc": "NBA Summer League in Las Vegas",
+        "links": {
+            "nba.com": "https://nbaevents.nba.com/events/nba-summer-league"
+        }
+    }
+}
