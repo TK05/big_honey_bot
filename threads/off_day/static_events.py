d05f7cdf336d8cb00657f293cef2ee0fdd48b813
Thomas Kelley <6148136+TK05@users.noreply.github.com>
2022-06-22 09:24:42 -0700

    update static events

--- threads/off_day/static_events.py
+++ threads/off_day/static_events.py
@@ -31,5 +31,41 @@ events = {
         "links": {
             "nba.com": "https://nbaevents.nba.com/events/nba-summer-league"
         }
+    },
+    189: {
+        "start_doy": 189,
+        "end_doy": 189,
+        "date_str": "July 8",
+        "desc": "Denver vs. Minnesota - 7:00PM - NBA TV - Summer League Game 1",
+        "links": {
+            "nba.com": "https://nbaevents.nba.com/events/nba-summer-league"
+        }
+    },
+    191: {
+        "start_doy": 191,
+        "end_doy": 191,
+        "date_str": "July 10",
+        "desc": "Denver vs. Cleveland - 5:00PM - ESPNU - Summer League Game 2",
+        "links": {
+            "nba.com": "https://nbaevents.nba.com/events/nba-summer-league"
+        }
+    },
+    194: {
+        "start_doy": 194,
+        "end_doy": 194,
+        "date_str": "July 13",
+        "desc": "Denver vs. LA Clippers - 8:00PM - NBA TV - Summer League Game 3",
+        "links": {
+            "nba.com": "https://nbaevents.nba.com/events/nba-summer-league"
+        }
+    },
+    196: {
+        "start_doy": 196,
+        "end_doy": 196,
+        "date_str": "July 15",
+        "desc": "Denver vs. Philadelphia - 4:00PM - NBA TV - Summer League Game 4",
+        "links": {
+            "nba.com": "https://nbaevents.nba.com/events/nba-summer-league"
+        }
     }
 }
