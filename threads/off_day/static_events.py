4d791c9e3aa3c8c5710ff4db8b168ea96766cc4c
Thomas Kelley <6148136+TK05@users.noreply.github.com>
2022-07-28 22:18:31 -0700

    update off season static events

--- threads/off_day/static_events.py
+++ threads/off_day/static_events.py
@@ -82,6 +82,56 @@ events = {
             "nba.com": "https://nbaevents.nba.com/events/nba-summer-league"
         }
     },
+    229: {
+        "start_doy": 229,
+        "end_doy": 229,
+        "date_str": "August 17",
+        "desc": "Serbia @ Slovenia | NT Friendly - 12:15AM MDT - Ljubjana, Slovenia",
+        "links": {
+            "local time": "https://dateful.com/time-zone-converter?t=12:15%20AM&tz=Denver&"
+        }
+    },
+    231: {
+        "start_doy": 231,
+        "end_doy": 231,
+        "date_str": "August 19",
+        "desc": "Serbia vs. Italy | NT Friendly - 10:00AM MDT - Hamburg, Germany",
+        "links": {
+            "local time": "https://dateful.com/time-zone-converter?t=10:00%20AM&tz=Denver&"
+        }
+    },
+    232.1: {
+        "start_doy": 232,
+        "end_doy": 232,
+        "date_str": "August 20",
+        "desc": "Serbia vs. Czech Republic | NT Friendly - 10:00AM MDT - Hamburg, Germany",
+        "links": {
+            "local time": "https://dateful.com/time-zone-converter?t=10:00%20AM&tz=Denver&"
+        }
+    },
+    232.2: {
+        "start_doy": 232,
+        "end_doy": 232,
+        "date_str": "August 20",
+        "desc": "Serbia @ Germany | NT Friendly - 12:30AM MDT - Hamburg, Germany",
+        "links": {
+            "local time": "https://dateful.com/time-zone-converter?t=12:30%20AM&tz=Denver&"
+        }
+    },
+    237: {
+        "start_doy": 237,
+        "end_doy": 237,
+        "date_str": "August 25",
+        "desc": "Serbia vs. Greece | World Cup Qualifier - Belgrade, Serbia",
+        "links": {}
+    },
+    240: {
+        "start_doy": 240,
+        "end_doy": 240,
+        "date_str": "August 28",
+        "desc": "Serbia @ Turkey | World Cup Qualifier - Istanbul, Turkey",
+        "links": {}
+    },
     252: {
         "start_doy": 252,
         "end_doy": 253,
