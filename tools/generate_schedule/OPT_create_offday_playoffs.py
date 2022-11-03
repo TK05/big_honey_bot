48a6292a18bcea28b80bfe98fa6ca0d0b09adbcb
TK05 <6148136+TK05@users.noreply.github.com>
2022-11-03 01:18:11 -0700

    create in season off day events

--- tools/generate_schedule/OPT_create_offday_playoffs.py
+++ tools/generate_schedule/OPT_create_offday_playoffs.py
@@ -11,6 +11,7 @@ from tools.generate_schedule.STEP3_create_calendar import update_calendar
 
 
 TIMEZONE = setup['timezone']
+INCLUDE_OFFDAYS = True
 platform_hr_min_fmt = "%#I:%M" if platform.system() == 'Windows' else '%-I:%M'
 platform_day_fmt = "%#d" if platform.system() == 'Windows' else '%-d'
 
@@ -82,17 +83,80 @@ def create_schedule(start, end, playoffs=True, count_down=False):
     return new_schedule
 
 
+def create_in_season_schedule(curr_sch):
+    days_with = []
+    days_without = []
+
+    for utc in curr_sch.keys():
+        dt = datetime.strptime(curr_sch[utc]['meta']['game_start'], "%m/%d/%y %I:%M %p")
+        days_with.append(dt.date())
+
+    for i, day in enumerate(days_with[:-1]):
+        diff = (days_with[i+1] - day).days
+        if diff > 0:
+            for n in range(1, diff):
+                days_without.append(day + timedelta(days=n))
+
+    pgpt = setup['pre_game_post_time'].split(':')
+    new_schedule = {}
+
+    for d in days_without:
+        post_time = datetime(
+            year=d.year,
+            month=d.month,
+            day=d.day,
+            hour=int(pgpt[0]),
+            minute=int(pgpt[1]),
+            tzinfo=ZoneInfo(TIMEZONE)
+        )
+        post_stamp = int(post_time.timestamp())
+
+        new_schedule[post_stamp] = {}
+        new_schedule[post_stamp]['meta'] = {}
+        new_schedule[post_stamp]['meta']['event_type'] = 'off'
+        loc_time_str = datetime.strftime(post_time, format(f'{platform_hr_min_fmt} %p'))
+        new_schedule[post_stamp]['meta']['post_time'] = loc_time_str
+
+        # Format Google Cal Event
+        new_schedule[post_stamp]['start'] = {}
+        new_schedule[post_stamp]['start']['dateTime'] = post_time.strftime('%G-%m-%dT%H:%M:%S')
+        new_schedule[post_stamp]['start']['timeZone'] = TIMEZONE
+        new_schedule[post_stamp]['end'] = {}
+        new_schedule[post_stamp]['end']['dateTime'] = (post_time + timedelta(seconds=61)).strftime('%G-%m-%dT%H:%M:%S')
+        new_schedule[post_stamp]['end']['timeZone'] = TIMEZONE
+        new_schedule[post_stamp]['location'] = 'Parts Unknown'
+
+        if d.weekday() == 4:
+            headline = f"Free Talk Friday | {d.strftime(f'%b {platform_day_fmt}, %Y')}"
+        else:
+            headline = f"Off-Day Discussion Thread | {d.strftime(f'%b {platform_day_fmt}, %Y')}"
+
+        new_schedule[post_stamp]['summary'] = headline
+        new_schedule[post_stamp]['description'] = f"{description_tags['daily_games']}"
+
+        new_schedule[post_stamp]['meta']['title_hash'] = create_hash(new_schedule[post_stamp]['summary'])
+        new_schedule[post_stamp]['meta']['body_hash'] = create_hash(new_schedule[post_stamp]['description'])
+
+    return new_schedule
+
+
 if __name__ == '__main__':
-    start_date = date(2022, 6, 17)
-    end_date = date(2022, 10, 19)
+    if INCLUDE_OFFDAYS:
+        with open('../json_output/all_events.json', 'r') as f:
+            raw_schedule = json.load(f)
+        schedule = create_in_season_schedule(raw_schedule)
+        update_calendar(schedule)
+    else:
+        start_date = date(2022, 6, 17)
+        end_date = date(2022, 10, 19)
 
-    schedule = create_schedule(start_date, end_date, playoffs=False, count_down=True)
-    update_calendar(schedule)
+        schedule = create_schedule(start_date, end_date, playoffs=False, count_down=True)
+        update_calendar(schedule)
 
-    try:
-        os.mkdir('../json_output')
-    except FileExistsError:
-        pass
+        try:
+            os.mkdir('../json_output')
+        except FileExistsError:
+            pass
 
-    with open('../json_output/off_day_events.json', 'w') as f:
-        json.dump(dict(sorted(schedule.items())), f, indent=4)
+        with open('../json_output/off_day_events.json', 'w') as f:
+            json.dump(dict(sorted(schedule.items())), f, indent=4)
