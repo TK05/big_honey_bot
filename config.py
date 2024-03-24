d146a9a68705073d9e592243b4d5e73f00f7d8b9
Tommy Kelley <6148136+TK05@users.noreply.github.com>
2024-03-23 17:37:19 -0700

    create global output path

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
+from pathlib import Path
+
 from dotenv import load_dotenv
 
 
+# TODO: cleanup env imports & usage
 load_dotenv()
 
 DEBUG = False
+OUTPUT_PATH = Path.cwd().joinpath("out")
 
 setup = {
     'season': 2023,  # Start year. EX: 18-19 season = 2018
@@ -42,16 +46,16 @@ setup = {
         'Referer': 'https://www.nba.com/',
         'Origin': 'https://stats.nba.com/'
     },
-    'flairs': {     # Subreddit specific link_flair IDs
-        'pre': '51a03fc6-3e0c-11e9-8856-0e97475b835e',
-        'game': 'b94d2b1e-7af0-11e8-9106-0ecef3c85830',
-        'post': '3f0b88d4-3e0c-11e9-afb3-0e6687893da2',
-        'off': '8bf4420e-4a77-11e9-a63c-0e1a3ad07cf6'
-    }
-    # 'flairs': {   # Test flairs
-    #     'pre': '701ba516-3e0d-11e9-80e7-0e07faed2e96',
-    #     'game': '7fa2da68-3e0d-11e9-9571-0e5fef20b374',
-    #     'post': '7962e238-3e0d-11e9-92dc-0edef102ea7a',
-    #     'off': 'e5a3688e-bde7-11ec-86e2-86c0ad65b04f'
+    # 'flairs': {     # Subreddit specific link_flair IDs
+    #     'pre': '51a03fc6-3e0c-11e9-8856-0e97475b835e',
+    #     'game': 'b94d2b1e-7af0-11e8-9106-0ecef3c85830',
+    #     'post': '3f0b88d4-3e0c-11e9-afb3-0e6687893da2',
+    #     'off': '8bf4420e-4a77-11e9-a63c-0e1a3ad07cf6'
     # }
+    'flairs': {   # Test flairs
+        'pre': '701ba516-3e0d-11e9-80e7-0e07faed2e96',
+        'game': '7fa2da68-3e0d-11e9-9571-0e5fef20b374',
+        'post': '7962e238-3e0d-11e9-92dc-0edef102ea7a',
+        'off': 'e5a3688e-bde7-11ec-86e2-86c0ad65b04f'
+    }
 }
