f4f2598166e8ab2bc014a03ffffa34b85c139563
TK05 <6148136+TK05@users.noreply.github.com>
2024-04-01 01:18:02 -0700

    refactor startup

--- start.py
+++ start.py
@@ -6,6 +6,15 @@ from big_honey_bot.config.main import OUTPUT_PATH, LOG_FILENAME, ENV_RELOAD_INTE
 from big_honey_bot.config.helpers import configure_logging, reload_env, get_env, get_pname_fname_str
 
 
+async def main():
+    # create thread to reload .env every ENV_RELOAD_INTERVAL_SEC
+    asyncio.create_task(reload_env(ENV_RELOAD_INTERVAL_SEC))
+
+    # start run_bhb main event loop
+    logger.info("Starting big_honey_bot....")
+    await run_bhb()
+
+
 if __name__ == "__main__":
     
     # setup logging
@@ -13,15 +22,10 @@ if __name__ == "__main__":
     configure_logging(log_file)
     logger = logging.getLogger(get_pname_fname_str(__file__))
 
-    logger.info("Starting big_honey_bot....")
-
-    # create thread to reload .env every ENV_RELOAD_INTERVAL_SEC
-    asyncio.create_task(reload_env(ENV_RELOAD_INTERVAL_SEC))
-
     if get_env('DEBUG'):
         from big_honey_bot.config.helpers import get_all_env
 
         logger.debug(f"All env variables: {get_all_env()}")
 
     # start run_bhb main event loop
-    asyncio.run(run_bhb())
+    asyncio.run(main())
