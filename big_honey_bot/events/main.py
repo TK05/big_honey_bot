887053e862c88a2d174c7c2fe49f94c0ff9e0447
TK05 <6148136+TK05@users.noreply.github.com>
2024-04-02 00:55:03 -0700

    migrate to asyncio

--- big_honey_bot/events/main.py
+++ big_honey_bot/events/main.py
@@ -42,12 +42,12 @@ def add_meta_and_body(event):
 async def get_all_events():
     service = await create_service()
 
-    return await service.get_events(order_by='startTime', single_events=True)
+    return service.get_events(order_by='startTime', single_events=True)
 
 
 async def get_all_events_with_meta():
     service = await create_service()
-    events = await service.get_events(order_by='startTime', single_events=True)
+    events = service.get_events(order_by='startTime', single_events=True)
     all_events = []
 
     for event in events:
@@ -60,7 +60,7 @@ async def get_all_events_with_meta():
 async def get_previous_event(penultimate=False, days=30):
     service = await create_service()
     now = get_datetime(to_tz=True, tz=setup['timezone'])
-    events = await service.get_events((now - timedelta(days=days)),now, order_by='startTime', timezone=setup['timezone'], single_events=True)
+    events = service.get_events((now - timedelta(days=days)),now, order_by='startTime', timezone=setup['timezone'], single_events=True)
 
     # Return penultimate event if requested, else return last event
     event = None
@@ -75,7 +75,7 @@ async def get_previous_event(penultimate=False, days=30):
 
 async def get_event(event_id):
     service = await create_service()
-    event = await service.get_event(event_id)
+    event = service.get_event(event_id)
     add_meta_and_body(event)
 
     return event
@@ -93,7 +93,7 @@ async def get_next_event():
 
 async def find_event(query, past=None):
     service = await create_service()
-    event = await next(service.get_events(time_min=past, query=query))
+    event = next(service.get_events(time_min=past, query=query))
     add_meta_and_body(event)
 
     return event
@@ -113,7 +113,7 @@ async def find_events_by_meta(**kwargs):
 
 async def create_event(event_data):
     service = await create_service()
-    await service.add_event(event_data)
+    service.add_event(event_data)
 
 
 async def update_event(event):
@@ -126,17 +126,17 @@ async def update_event(event):
     # Update description w/ new hashes and any body changes
     event.description = f"{description_tags['meta_start']}{json.dumps(event.meta)}{description_tags['meta_end']}" \
                         f"{description_tags['body_start']}{event.body}{description_tags['body_end']}"
-    await service.update_event(event)
+    service.update_event(event)
 
 
 async def update_event_serial(event_json):
     service = await create_service()
-    await service.update_event(EventSerializer.to_object(event_json))
+    service.update_event(EventSerializer.to_object(event_json))
 
 
 async def delete_event(event):
     service = await create_service()
-    await service.delete_event(event)
+    service.delete_event(event)
 
 
 async def delete_all_events():
