a6f33301be140d43e9509b9ed9474da785a781a5
TK05 <6148136+TK05@users.noreply.github.com>
2025-10-14 21:08:45 -0700

    add more headline template strings

--- big_honey_bot/threads/static/headlines.py
+++ big_honey_bot/threads/static/headlines.py
@@ -19,7 +19,12 @@ headlines = {
         "4": [
             "{0} win a close one over the {1} {2} | {3}",
             "{0} squeeze by the {1} {2} | {3}",
-            "{0} narrowly edge out the {1} {2} | {3}"
+            "{0} sneak past the {1} {2} | {3}",
+            "{0} outlast the {1} {2} | {3}",
+            "{0} narrowly defeat the {1} {2} | {3}",
+            "{0} claim close win over the {1} {2} | {3}",
+            "{0} get the better of the {1} {2} | {3}",
+            "{0} slip by the {1} {2} | {3}",
         ],
         "15": [
             "{0} beat the {1} {2} | {3}",
@@ -29,7 +34,13 @@ headlines = {
             "{0} master the {1} {2} | {3}",
             "{0} humble the {1} {2} | {3}",
             "{0} overpower the {1} {2} | {3}",
-            "{0} put the {1} in their place {2} | {3}"
+            "{0} put the {1} in their place {2} | {3}",
+            "{0} top the {1} {2} | {3}",
+            "{0} take down the {1} {2} | {3}",
+            "{0} prevail over the {1} {2} | {3}",
+            "{0} handle the {1} {2} | {3}",
+            "{0} outshine the {1} {2} | {3}",
+            "{0} get it done against the {1} {2} | {3}",
         ],
         "100": [
             "{0} conquer the {1} {2} | {3}",
@@ -38,24 +49,48 @@ headlines = {
             "{0} clobber the {1} {2} | {3}",
             "{0} make mincemeat of the {1} {2} | {3}",
             "{0} wipe the floor with the {1} {2} | {3}",
-            "{0} rout the {1} {2} | {3}"
+            "{0} rout the {1} {2} | {3}",
+            "{0} cruise past the {1} {2} | {3}",
+            "{0} overpower the {1} {2} | {3}",
+            "{0} rout the {1} {2} | {3}",
+            "{0} dominate the {1} {2} | {3}",
+            "{0} blow past the {1} {2} | {3}",
+            "{0} roll over the {1} {2} | {3}",
+            "{0} dismantle the {1} {2} | {3}",
+            "{0} outclass the {1} {2} | {3}",
         ]
     },
     "0": {
         "4": [
             "{0} drop a close one to the {1} {2} | {3}",
             "{0} fall short against the {1} {2} | {3}",
-            "{0} let one slip away against the {1} {2} | {3}"
+            "{0} let one slip away against the {1} {2} | {3}",
+            "{0} come up short against the {1} {2} | {3}",
+            "{0} unable to top the {1} {2} | {3}",
+            "{0} outlasted by the {1} {2} | {3}"
         ],
         "15": [
             "{0} fall to the {1} {2} | {3}",
             "{0} stuffed by the {1} {2} | {3}",
-            "{0} lose one to the {1} {2} | {3}"
+            "{0} lose one to the {1} {2} | {3}",
+            "{0} outplayed by the {1} {2} | {3}",
+            "{0} beaten by the {1} {2} | {3}",
+            "{0} overpowered by the {1} {2} | {3}",
+            "{0} lose to the {1} {2} | {3}",
+            "{0} come up empty against the {1} {2} | {3}",
+            "{0} can’t keep pace with the {1} {2} | {3}",
         ],
         "100": [
             "{0} humbled by the {1} {2} | {3}",
             "{0} clobbered by the {1} {2} | {3}",
-            "{0} subdued by the {1} {2} | {3}"
+            "{0} subdued by the {1} {2} | {3}",
+            "{0} routed by the {1} {2} | {3}",
+            "{0} crushed by the {1} {2} | {3}",
+            "{0} dominated by the {1} {2} | {3}",
+            "{0} blown out by the {1} {2} | {3}",
+            "{0} outclassed by the {1} {2} | {3}",
+            "{0} overwhelmed by the {1} {2} | {3}",
+            "{0} overrun by the {1} {2} | {3}",
         ]
     }
 }
